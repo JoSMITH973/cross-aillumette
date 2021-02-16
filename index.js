86361940fcab47a990e5f493b64eb5000d45e1d1
JoSMITH973 <72966617+JoSMITH973@users.noreply.github.com>
2021-02-16 10:08:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let line1 = "IIIIIII"
+console.log('*****\n* I *\n*III*\n*****')
+rl.question('Choisissez Une ligne:', (name)=>{
+    
+})
